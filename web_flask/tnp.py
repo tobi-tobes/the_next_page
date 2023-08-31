b9bd94f78a0ddb62bccb9eb4c08cf64c2a574fa6
Tobi Tijani <tobi_tijani@yahoo.com>
2023-08-31 09:57:19 -0700

    Added caching to template files

--- web_flask/tnp.py
+++ web_flask/tnp.py
@@ -2,6 +2,7 @@
 """ Starts a Flask Web Application """
 from flask import Flask, render_template
 from models import storage
+import uuid
 
 app = Flask(__name__)
 
@@ -15,13 +16,13 @@ def close_db(error):
 @app.route('/the_next_page', strict_slashes=False)
 def tnp_home():
     """ The Next Page is Alive!!! """
-    return render_template('home.html')
+    return render_template('home.html', cache_id=uuid.uuid4())
 
 
 @app.route('/the_next_page/about', strict_slashes=False)
 def tnp_about():
     """ About the contributors """
-    return render_template('about.html')
+    return render_template('about.html', cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
